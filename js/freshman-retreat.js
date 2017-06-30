adca025b4864d1df5d8e7da0859e4f8fd24ceeba
Anthony Luc <anthonyvanluc@gmail.com>
2017-06-30 19:28:15 -0400

    added smoothscrolling

--- js/freshman-retreat.js
+++ js/freshman-retreat.js
@@ -13,3 +13,32 @@ function loadIframe(obj) {
   obj.style.width =  width + 'px';
   obj.style.height = width*9/16 + 'px';
 }
+
+/* Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        navBarHeight = document.getElementById("navBar").clientHeight
+        scrollHeight = target.offset().top-navBarHeight-15 // 15 for padding
+        $('html, body').animate({
+          scrollTop: scrollHeight
+        });
+      }
+    }
+  });
