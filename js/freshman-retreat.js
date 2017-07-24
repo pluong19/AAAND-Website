1cdc1ccac699ebbe547a43769ff02e7a2229b201
Anthony Luc <anthonyvanluc@gmail.com>
2017-07-23 21:51:14 -0400

    done with site (just need yt link)

--- js/freshman-retreat.js
+++ js/freshman-retreat.js
@@ -1,36 +1,31 @@
-// $(document).ready(function(){
-//
-// });
 
-$("#welcomeNav").click(function() {
-    $('html, body').animate({ scrollTop:$("#welcome").offset().top}, 500);
-});
 
 /* Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 // Select all links with hashes
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        navBarHeight = document.getElementById("navBarScrollID").clientHeight
-        scrollHeight = target.offset().top-navBarHeight
-        $('html, body').animate({
-          scrollTop: scrollHeight
-        });
-      }
+
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      navBarHeight = document.getElementById("navBarScrollID").clientHeight
+      scrollHeight = target.offset().top-navBarHeight-67 // Adjust '67' when the animation does not bring it right up to the header.
+      $('html, body').animate({
+        scrollTop: scrollHeight
+      });
     }
-  });
+  }
+});
